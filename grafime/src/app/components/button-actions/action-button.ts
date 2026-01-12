cc26af9bc58a76159e15299bb6328738f1f969f2
Rodevp <rodriguezrodrigor12@gmail.com>
2026-01-12 10:35:19 -0500

    estilos a las acciones listo

--- grafime/src/app/components/button-actions/action-button.ts
+++ grafime/src/app/components/button-actions/action-button.ts
@@ -1,8 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, input } from "@angular/core";
 
 @Component({
     selector: 'app-action-button',
     templateUrl: './action-button.html',
     styleUrls: ['./action-button.css']
 })
-export class ActionButtonComponent { }
+export class ActionButtonComponent {
+    text = input<string>();
+}
